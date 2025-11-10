eec99c69c8af36b3b9f93a83a7da787b0e6c5c93
Arturo Candela <a.candelamolto@edu.gva.es>
2025-11-10 11:47:55 +0100

    [MOD] empezamos a trabajar con componentes Blade

--- src/resources/views/landing/services.blade.php
+++ src/resources/views/landing/services.blade.php
@@ -4,5 +4,22 @@
     <h1>Servicios</h1>
 @endsection
 @section('content')
-    <p>Descripción de nuestros servicios.</p>
+    @component('_components.card')
+        @slot('title', 'Servicio 1');
+        @slot('content')
+            <p>Descripción breve del servicio 1.</p>
+        @endslot
+    @endcomponent
+    @component('_components.card')
+        @slot('title', 'Servicio 2');
+        @slot('content')
+            <p>Descripción breve del servicio 2.</p>
+        @endslot
+    @endcomponent
+    @component('_components.card')
+        @slot('title', 'Servicio 3');
+        @slot('content')
+            <p>Descripción breve del servicio 3.</p>
+        @endslot
+    @endcomponent
 @endsection
