760ce5cb69cc778b9e4eefad5f01f5c83b1e1d16
Arturo Candela <a.candelamolto@edu.gva.es>
2025-11-10 08:16:13 +0100

    [ADD] Finalizada TC2

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,5 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'landing.index')-> name('index');
-Route::view('/about', 'landing.about')-> name('about');
+Route::view('/', 'home')->name('home');
+Route::view('/about', 'about')->name('about');
+Route::view('/contact', 'contact')->name('contact');
