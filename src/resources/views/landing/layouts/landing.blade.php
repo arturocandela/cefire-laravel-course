30c534ac63bf4b129e3c1e9a42971bb4c96fd721
Arturo Candela <a.candelamolto@edu.gva.es>
2025-11-10 08:37:37 +0100

    Primeras pruebas con el uso de las plantillas

---
+++ src/resources/views/landing/layouts/landing.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>
+        @yield('title')
+    </title>
+</head>
+<body>
+    <header>
+        @yield('header')"
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+
+    <footer>
+        <p>Pie de página &copy; 2025</p>
+    </footer>
+</body>
+</html>
