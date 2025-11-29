04d19ba195bb3796ca77973f200f061072352b1e
Arturo Candela <a.candelamolto@edu.gva.es>
2025-11-29 22:21:07 +0100

    Trabajando con un agente y creando parte de las vistas

---
+++ src/resources/views/notes/show.blade.php
@@ -0,0 +1,2 @@
+<h1>Detalle de Nota</h1>
+<p>El ID de la nota es: {{ $id }}</p>
--- src/routes/web.php
+++ src/routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\EmployeeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\NoteController;
+use App\Http\Controllers\ProductController;
 
 
 Route::get('/', [UserController::class, 'index'])->name('user.index');
@@ -11,3 +13,5 @@ Route::get('/employees/by-id', [EmployeeController::class, 'byId'])->name('emplo
 Route::get('/employees/by-lastname', [EmployeeController::class, 'byLastName'])->name('employees.byLastName');
 Route::get('/employees/filter-letter', [EmployeeController::class, 'lastNameStartsWith'])->name('employees.lastNameStartsWith');
 Route::get('/employees/filter-year', [EmployeeController::class, 'bornIn'])->name('employees.bornIn');
+
+Route::resource('product', ProductController::class);
